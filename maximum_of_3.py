65f1e42e3699c0e8514609ce07d016d0392e7a79
Ronit Gavaskar <rgavaska@purdue.edu>
2017-08-08 03:01:01 -0700

    Add files via upload

---
+++ maximum_of_3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+Implement a function that takes as input three variables, and returns the
+largest of the three. Do this without using the Python max() function!
+
+The goal of this exercise is to think about some internals that Python
+normally takes care of for us. All you need is some variables and if statements!
+"""
+
+
+x = int(raw_input('Enter your first number: '))
+current_max = x
+
+y = int(raw_input('Enter your second number: '))
+if y > x:
+    current_max = y
+
+z = int(raw_input('Enter your third number: '))
+if z > current_max:
+    current_max = z
+
+print('%d is the maximum of three numbers' % current_max)
